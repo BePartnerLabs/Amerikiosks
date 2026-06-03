a83af89b686fa8d2542df6ced84c29e923452ea6
Giorgiosaud <giorgiosaud@gmail.com>
2026-06-03 19:16:44 -0400

    feat(ui): implement CTA block, ArchiveBlock, CollectionArchive, and Card styles

--- src/blocks/CallToAction/Component.tsx
+++ src/blocks/CallToAction/Component.tsx
@@ -1,34 +1,46 @@
 import type React from 'react'
 import { CMSLink } from '@/components/Link'
-
 import RichText from '@/components/RichText'
 import type { CallToActionBlock as CTABlockProps } from '@/payload-types'
+import { toSnakeCase } from '@/utilities/toSnakeCase'
+import './styles.css'
 
-export const CallToActionBlock: React.FC<CTABlockProps> = ({ links, richText }) => {
+export const CallToActionBlock: React.FC<CTABlockProps> = ({
+  links,
+  richText,
+  blockName,
+  blockType,
+}) => {
   return (
-    <div className="">
-      <div className="">
-        <div className="">
+    <section
+      className="ak-cta"
+      data-ga-block={toSnakeCase(blockType)}
+      data-ga-section={blockName ?? undefined}
+    >
+      <div className="bp-content-grid">
+        <div className="breakout ak-cta__inner">
           {richText && (
-            <RichText
-              className=""
-              data={richText}
-              enableGutter={false}
-            />
+            <div className="ak-cta__text">
+              <RichText
+                data={richText}
+                enableGutter={false}
+              />
+            </div>
+          )}
+          {(links || []).length > 0 && (
+            <div className="ak-cta__actions">
+              {(links || []).map(({ link }) => (
+                <CMSLink
+                  key={`${link.type}-${link.url}-${link.label}`}
+                  size="lg"
+                  {...link}
+                  data-ga-event="cta_click"
+                />
+              ))}
+            </div>
           )}
         </div>
-        <div className="">
-          {(links || []).map(({ link }) => {
-            return (
-              <CMSLink
-                key={`${link.type}-${link.url}-${link.label}`}
-                size="lg"
-                {...link}
-              />
-            )
-          })}
-        </div>
       </div>
-    </div>
+    </section>
   )
 }
